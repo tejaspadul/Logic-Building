6e9462937daa24bad9e2c170bcc11fdba2a3af5c
Tejas <your_email@tejaspadul.com>
2026-06-18 21:14:41 +0530

    Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Fibonacci {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of terms for Fibonacci series: ");
+        int n = sc.nextInt();
+        int a=0;
+        int b=1;
+        for(int i=1;i<=n;i++){
+            System.out.print(a+" ");
+            int c=a+b;
+            a=b;
+            b=c;
+        }
+    }
+}
