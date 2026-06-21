a27359b8a4e098d30df31fcc8cc50bb4decdbbf7
tejas <163300834+tejaspadul@users.noreply.github.com>
2026-06-21 22:14:01 +0530

    Add explanation comment for largest number logic
    Added a comment explaining the logic to find the largest number among three numbers.

--- LargestNumber.java
+++ LargestNumber.java
@@ -26,3 +26,9 @@ public class LargestNumber {
             sc.close();
         }
     }
+
+/*
+to fing largest number among three number is that we compare three number with each other and print greatest among them
+ex- 12,23,34  here we compare 12 with 23,34 is greater or not then we compare 23 with 12,34 greater or not if first two codition are not satisfied 
+then third will be definetly true so here 34 is greatest
+*/
